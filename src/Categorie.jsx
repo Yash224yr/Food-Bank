f17b380126db7612f0d117a63e7eadfe86158ec5
yash <yash224.yr@gmail.com>
2023-06-13 14:01:06 +0530

    commit

--- src/Categorie.jsx
+++ src/Categorie.jsx
@@ -1,7 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { mealcontext } from './App';
 
 
@@ -43,14 +42,7 @@ function Categorie() {
                 {categories.map((meal, index) => (
                     <div key={index} className='category-item'>
                         <img className='imggoto' src={meal.strCategoryThumb} alt="" />
-                        <h1 className='goto'>{meal.strCategory}</h1>
-                        <div className='icons'>
-                            <h1><Link>{meal.strCategory}</Link></h1>
-                            <div className='iconlogo'>
-                                <FavoriteBorderIcon></FavoriteBorderIcon>
-                            </div>
-                            <button onClick={()=>{getdescription(meal.strCategoryDescription)}} ><Link to={`/list/${meal.strCategory}`}>Explore</Link></button>
-                        </div>
+                        <h1 className='goto'  onClick={()=>{getdescription(meal.strCategoryDescription)}}><Link to={`/list/${meal.strCategory}`}>{meal.strCategory}</Link></h1>
                     </div>
                 ))}
             </div>
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,10 +3,14 @@ import FoodBankIcon from '@mui/icons-material/FoodBank';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import CloseIcon from '@mui/icons-material/Close';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+
+
 
 function Header() {
   const [random, setRandom] = useState('');
   const [check, setCheck] = useState(false);
+  const [menu , setMenu] = useState(true)
 
   const getRandomMeal = () => {
     axios
@@ -32,12 +36,16 @@ function Header() {
         <FoodBankIcon className='logo-icon' style={{ color: 'white' }} />
         <h1>Food Bank</h1>
       </div>
-      <div className='headerright'>
+      <div className='headerright'  style={{ top: menu===false ? "10%" : "-100%" }} >
         <Link to='/'>Home</Link>
         <Link to='/ingre'>Ingredients</Link>
         <Link to='/area'>Regional Delicacies</Link>
         <Link to="/fav">Favourite</Link>
       </div>
+      <div className='menu'>
+      <a href="" onClick={(e)=>{e.preventDefault() ; setMenu(!menu)}} ><MenuBookIcon></MenuBookIcon></a>
+      </div>
+
       <div className='get-random'>
         <button onClick={handleRandomMealClick}>  {check ? (<CloseIcon/>) : ("Random Meal") } </button>
         {random && check && (
