34631e53fcd7956c1ee40141df3adccce821a642
yash <yash224.yr@gmail.com>
2023-06-28 03:26:21 +0530

    commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from 'react';
-import FoodBankIcon from '@mui/icons-material/FoodBank';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import CloseIcon from '@mui/icons-material/Close';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
+import SearchIcon from '@mui/icons-material/Search';
+
 
 
 
 function Header() {
   const [random, setRandom] = useState('');
   const [check, setCheck] = useState(false);
-  const [menu , setMenu] = useState(true)
+  const [menu, setMenu] = useState(true)
+
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleBurgerClick = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
+  const handleMenuLinkClick = () => {
+    setIsMenuActive(false);
+  };
 
   const getRandomMeal = () => {
     axios
@@ -31,29 +40,55 @@ function Header() {
   };
 
   return (
-    <header>
-      <div className='headerleft'>
-        <FoodBankIcon className='logo-icon' style={{ color: 'white' }} />
-        <h1>Food Bank</h1>
-      </div>
-      <div className='headerright'  style={{ top: menu===false ? "10%" : "-100%" }} >
-        <Link to='/'>Home</Link>
-        <Link to='/ingre'>Ingredients</Link>
-        <Link to='/area'>Regional Delicacies</Link>
-        <Link to="/fav">Favourite</Link>
-      </div>
-      <div className='menu'>
-      <a href="" onClick={(e)=>{e.preventDefault() ; setMenu(!menu)}} ><MenuBookIcon></MenuBookIcon></a>
-      </div>
-
-      <div className='get-random'>
-        <button onClick={handleRandomMealClick}>  {check ? (<CloseIcon/>) : ("Random Meal") } </button>
-        {random && check && (
-          <a href={`/receipe/${random}`} className="hidden-link">
-            Go to Random Meal
-          </a>
-        )}
-      </div>
+    <header className="header" id="header">
+      <nav className="navbar container">
+        <a href="#" className="brand">
+          Brand
+        </a>
+        <div className="search">
+          <form className="search-form">
+            <input
+              type="text"
+              name="search"
+              className="search-input"
+              placeholder="Search for Meal"
+              autoFocus
+            />
+            <button type="submit" className="search-submit" disabled>
+              <SearchIcon></SearchIcon>
+            </button>
+          </form>
+        </div>
+        <div className={`menu ${isMenuActive ? 'is-active' : ''}`} id="menu">
+          <ul className="menu-inner">
+            <li className="menu-item">
+              <a href="#" className="menu-link" onClick={handleMenuLinkClick}>
+                Listing
+              </a>
+            </li>
+            <li className="menu-item">
+              <a href="#" className="menu-link" onClick={handleMenuLinkClick}>
+                Feature
+              </a>
+            </li>
+            <li className="menu-item">
+              <a href="#" className="menu-link" onClick={handleMenuLinkClick}>
+                Popular
+              </a>
+            </li>
+            <li className="menu-item">
+              <a href="#" className="menu-link" onClick={handleMenuLinkClick}>
+                Support
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="burger" id="burger" onClick={handleBurgerClick}>
+          <span className="burger-line"></span>
+          <span className="burger-line"></span>
+          <span className="burger-line"></span>
+        </div>
+      </nav>
     </header>
   );
 }
